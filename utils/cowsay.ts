39c87ae3d384566696f53735f6e2f301c1c6b0ea
Tarun Balaji Ravi <tarunbalaji3@gmail.com>
2022-03-23 12:44:26 -0400

    Add random quote functionality

--- utils/cowsay.ts
+++ utils/cowsay.ts
@@ -1,9 +1,23 @@
 import * as cowsay from 'cowsay';
 import getRandomInt from './random';
+import quotes from './quotes.json';
+
+/*
+Get random number from getRandomInt
+Use that number to get a specific object in the quotes array
+Use that object to access the properties quote and author
+Format the value of the quote and author key to a string
+Pass the string into default function
+*/
+
+const rand = getRandomInt(0, 25);
+const val = quotes[rand];
+const quoteMessage = val.quote;
+const author = val.author;
 
 export default function () {
   let output: string = cowsay.say({
-    text: 'Hello from typescript!',
+    text: `${quoteMessage} - ${author}`,
     e: '^^',
     T: 'U ',
     r: true,
